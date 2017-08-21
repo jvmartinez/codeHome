6014f492e6be472b8aa3ae45d0c4e88bb4ce8a08
Juan Martinez <juanmartinez.cslcv@gmail.com>
2017-08-21 10:05:50 -0400

    general change

--- codeHome.py
+++ codeHome.py
@@ -2,20 +2,16 @@
 from array import array
 import setting.Constant as constant
 import lib.create as create
-
-# comand = "create class juan _nombre _apellido _sexo _edad :saludar *x *y :despedir"
-# comand = "create component table tableUser #miTablee -3 col1,col2,col3 -2"
-
 print "" \
       "Bienvenido a CodeHome\n" \
       "Version: 0.1\n" \
       "Desarrollador: Juan Martinez\n" \
       "Framewok:\n CodeHome esta destinado para facilitar la construccion de estructura" \
       "en php.\n" \
-      "Usabilidad:\n Comando codeHelp es breve manual de ayuda para construir tus clases en php\n" \
+      "Usabilidad:\n Comando codeHelp-es(Spanish)/codeHepl-en(English) breve guia de ayuda para construir tus clases en php\n" \
       "*************************************************************************************\n" \
       ""
-comand = raw_input("Instruccion a ejecutar: ")
+comand = raw_input("Instruction to execute: ")
 tpVariables = comand
 action = tpVariables.split(" ")[0]
 tpVariables = tpVariables.split("_")
@@ -32,27 +28,24 @@ def infoApp():
     document.close()
     return contents
 
-if action == "codeHelp":
-    print "Construccion de clases :\n Comienza(create class) segido por el nombre de la clase\n"
-    print "Construccion de variables:\n Comienza con (_) segido por el nombre de la variable\n"
-    print "Construccion de metodos sin parametros:\n Comienza con (:) segido por el nombre del metodo\n"
-    print "Construccion de metodos con parametros:\n Comienza con (:) segido por el nombre del metodo y las variables \n identificada por * segida por su nombre\n"
+if action == "codeHelp-es":
+    print "Construccion de clases :\n Comienza (create class) seguido por el nombre de la clase\n"
+    print "Construccion de variables:\n Comienza con (_) seguido por el nombre de la variable\n"
+    print "Construccion de metodos sin parametros:\n Comienza con (:) seguido por el nombre del metodo\n"
+    print "Construccion de metodos con parametros:\n Comienza con (:) seguido por el nombre del metodo y las variables \n identificada por * segida por su nombre\n"
     print "Ejemplo :create class Juan _name _lastName _sex _age :greet *friend :bye\n"
-else:
+
+if action =="codeHelp-en":
+    print "Class construction :\n Begins(create class) followed by the name of the class\n"
+    print "Variables construction:\n Begins con (_) followed by the name of the variable\n"
+    print "Method construction without parameters:\n Begins con (:) followed by the name of the method\n"
+    print "Method construction with parameters:\n Begins con (:) followed by the name of the method and variables \n identified by * followed by name\n"
+    print "Example :create class Juan _name _lastName _sex _age :greet *friend :bye\n"
+else :
     if action != "create" and action != "update" and action != "delete" and action != "create-controller":
         print "Verifique que sus instrucciones sean correctas!"
         print "Ayuda instrodusca codeHome help!"
     else:
         if action.endswith("create"):
             create.create(operator, variables, actionFunctions, "none", "none")
-        if action.endswith("update"):
-            print "actualizando"
-        if action.endswith("delete"):
-            print "eliminado"
-        if action.endswith("create-controller"):
-            create.create(operator, variables, actionFunctions, infoApp(), constant.controller())
-        if action.endswith("create-model"):
-            create.create(operator, variables, actionFunctions, infoApp(), constant.model())
-        if action.endswith("create-view"):
-            create.create(operator, variables, actionFunctions, infoApp(), constant.view())
-        print "proceso con exito...."
+        print "Process with success...."
